e4c775e834c23ab07c020fb3058be90b53115233
Mohamedlemin <med3744lemin@gmail.com>
2022-01-13 01:22:29 +0000

    Restart

--- source/ChefService/dao/ChefServicedao.java
+++
@@ -1,8 +0,0 @@
-package ChefService.dao;
-
-import ChefService.metier.ChefService;
-
-public interface ChefServicedao {
-	public ChefService login(String user,String password);
-
-}
--- source/ChefService/dao/servicedao.java
+++
@@ -1,9 +0,0 @@
-package ChefService.dao;
-
-import ChefService.metier.Service;
-
-public interface servicedao {
-	
-	public Service getService(Long id_chef);
-
-}
